a8cd529002cf0c221a7578f448b81cad69fbac69
Michael <m.colandrea@outlook.com>
2024-06-20 13:01:34 -0400

    Added /release_date/year to grab games by year

--- app/routers/release_dates.py
+++ app/routers/release_dates.py
@@ -33,8 +33,40 @@ def get_all_games_by_release_date():
         cur.close()
         conn.close()
         
+@router.get("/release_date/{year}", tags=["Release Date"])
+def get_game_by_release_date_by_year(year: str):
+    # Grab release date from a range of years
+    try: 
+        conn,cur = connect_db()
+        
+    # If connection fails, return an error message
+    except:
+        return {"Error" : "Connection to database failed!"}
+
+    # Try to get the games that have a name similar to the query
+    try:
+     
+        sql_query = " SELECT * FROM api.game_info WHERE EXTRACT(YEAR FROM TO_DATE(release_date, 'MM-DD-YYYY')) = %s ORDER BY TO_DATE(release_date, 'MM-DD-YYYY') ASC" 
+        params = (year)
+        cur.execute(sql_query, (params,))
+        result = cur.fetchall() 
+        
+        # If no games are found, return an error message
+        if not result:
+            return {"Error" : "No Games Found!"}
+        return {"game_info": result}
+    
+    # If an error occurs, return the error message
+    except Exception as e:
+        return {"Error" : str(e)}
+    finally:
+        cur.close()
+        conn.close()
+            
+        
+        
 @router.get("/release_date/{year1}/{year2}", tags=["Release Date"])
-def get_game_by_release_date(year1: str, year2: str):
+def get_game_by_release_date_by_range(year1: str, year2: str):
     # Grab release date from a range of years
     try: 
         conn,cur = connect_db()
