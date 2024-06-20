37641e0ae639e0a78357518b5db3d83dbf16e832
Michael <m.colandrea@outlook.com>
2024-06-20 00:56:06 -0400

    Added /developers/name to search games by developers by name

--- app/routers/developers.py
+++ app/routers/developers.py
@@ -30,3 +30,28 @@ def get_games_by_developers():
     finally:
         cur.close()
         conn.close()
+        
+@router.get("/developers/{name}", tags=["Developers"])
+def get_games_by_developers_name(name: str):
+    search_query = f"%{name}%"
+    try:
+        conn,cur = connect_db()
+    except:
+        raise HTTPException(status_code=500, detail="Connection to database failed!")
+    
+    try:
+        sql_query = "SELECT developer, ARRAY_AGG(game_title) AS games FROM api.game_info where developer ILIKE %s GROUP BY developer ORDER BY developer ASC"
+        params = (search_query)
+        cur.execute(sql_query, (search_query,))
+        conn.commit()
+        result = cur.fetchall()
+            
+        if not result:
+           raise HTTPException(status_code=404, detail="No Games Found!")
+        return {"game_info": result}
+        
+    except:
+       raise HTTPException(status_code=500, detail="An error occurred!")
+    finally:
+        cur.close()
+        conn.close()
